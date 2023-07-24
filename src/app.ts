db0dff9bc431eebaf68729d5da1464b3b4c96d7a
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-07-24 16:17:26 +0600

    Finest Server Setup

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-const port = 3000;
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
 // Corse
-app.use(cors());
+app.use(cors())
 
 // Parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("✔✌Lit-books Server running........");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('✔✌Lit-books Server running........')
+})
 
-export default app;
+export default app
